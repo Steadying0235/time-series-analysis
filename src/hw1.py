cedc3c7797d038f219231a3959cdb4705fba48fb
steven <github@arrownoon.maskmy.id>
2024-02-01 17:28:50 -0600

    add dataset

--- src/hw1.py
+++ src/hw1.py
@@ -2,6 +2,9 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
+
+import os
 
 
 #
@@ -12,16 +15,29 @@ import matplotlib.pyplot as plt
 # Returns:
 #  None
 #
-def plot_standard_t(df, length) -> None:
-    df = 5
-    length = 48
-    v = np.random.default_rng().standard_t(df, length)
-    plt.plot([i for i in range(length)], v, marker="o")
+def plot_standard_t(df, size) -> None:
+    v = np.random.default_rng().standard_t(df, size)
+    plt.plot([i for i in range(size)], v, marker="o")
+    plt.show()
+
+
+def plot_dubuque_temp(data: pd.DataFrame, value: str) -> None:
+    plt.plot(data.index, data[value], marker="o")
+    plt.ylabel("Dubuque Temperature")
+    plt.xlabel("Time")
     plt.show()
 
 
 def main():
-    q1_5()
+    PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    DATASETS = os.path.join(PROJECT_ROOT, "datasets")
+    # q1.5
+    plot_standard_t(5, 48)
+
+    # q1.6
+    # tempdub = os.path.join(DATASETS, "tempdub.dat")
+    # tempdub = pd.read_csv(tempdub)
+    # plot_dubuque_temp(tempdub, "tempdub")
 
 
 if __name__ == "__main__":
